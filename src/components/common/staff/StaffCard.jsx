9f6a29e155ecafedd790f4326c802f67fa0a27a2
Hem Sothearo <hem.sothearo.1223@rupp.edu.kh>
2026-01-29 19:28:39 +0700

    StaffCard component Added

---
+++ src/components/common/staff/StaffCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { MdOutlineModeEdit } from 'react-icons/md';
+import { FaRegTrashCan } from 'react-icons/fa6';
+
+
+function StaffCard({ user }) {
+  if (!user) return null;
+  const hireDate = user.hireDate instanceof Date ? user.hireDate : new Date(user.hireDate);
+  
+  return (
+        <div key={user.id} className='bg-white border border-blue-200 rounded-lg shadow-md p-4 items-center space-x-4'>
+              <div className='flex'>
+                <img src={user.avatar || 'https://via.placeholder.com/150'} alt={user.name} className='w-16 h-16 rounded-full object-cover mb-4' />
+                <div className='flex flex-col ml-4'>
+                  <h2 className='text-lg font-semibold text-blue-900'>{user.name}</h2>
+                  <div className='flex gap-2'>
+                    <p className='text-gray-900 bg-blue-300 px-2 text-sm rounded-lg py-0.5'>{user.role?.charAt(0).toUpperCase() + user.role?.slice(1)}</p>
+                    <p className='text-gray-900 bg-green-300 px-2 text-sm rounded-lg py-0.5'>{user.status}</p>
+                  </div>
+                </div>
+              </div>
+              <div className='mt-7 space-y-2'>
+                <p className='text-gray-600 text-sm'><span className='text-sm'>Email:</span> {user.email}</p>
+                <p className='text-gray-600 text-sm'><span className='text-sm'>Phone:</span> {user.phone}</p>
+                <p className='text-gray-600 text-sm'><span className='text-sm'>Hire Date:</span> {hireDate.toLocaleDateString()}</p>
+              </div>
+              <div className='mt-4 flex'>
+                <button className='w-70 border border-blue-200 rounded-md px-4 py-1 mr-2 hover:bg-blue-300'>Deactivate</button>
+                <button className='border border-blue-200 rounded-md px-4 py-1 mr-2 hover:bg-blue-300'><MdOutlineModeEdit /></button>
+                <button className='border border-red-300 rounded-md px-4 py-1 text-red-500 hover:bg-red-300'><FaRegTrashCan /></button>
+              </div>
+        </div>
+  )
+}
+
+export default StaffCard
--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -1,11 +1,9 @@
-import { PlusIcon } from 'lucide-react'
+import { Key, PlusIcon } from 'lucide-react'
 import StaffInfoCard from '../components/common/staff/StaffInfoCard.jsx'
 import StasCrad from '../components/common/staff/StaffCard.jsx'
-import React from 'react'
 import { useContext } from 'react'
 import { AuthContext } from '../contexts/AuthContext.jsx';
-import { MdOutlineModeEdit } from 'react-icons/md';
-import { FaRegTrashCan } from 'react-icons/fa6';
+import StaffCard from '../components/common/staff/StaffCard.jsx';
 
 function Staff() {
   const {users} = useContext(AuthContext);
@@ -23,30 +21,9 @@ function Staff() {
         <div className='w-7xl grid grid-cols-3 gap-6'>
           {
           users?users.map((user) => (
-            <div key={user.id} className='bg-white border border-blue-200 rounded-lg shadow-md p-4 items-center space-x-4'>
-              <div className='flex'>
-                <img src={user.avatar} alt={user.name} className='w-16 h-16 rounded-full object-cover mb-4' />
-                <div className='flex flex-col ml-4'>
-                  <h2 className='text-lg font-semibold text-blue-900'>{user.name}</h2>
-                  <div className='flex gap-2'>
-                    <p className='text-gray-900 bg-blue-300 px-2 text-sm rounded-lg py-0.5'>{user.role.charAt(0).toUpperCase() + user.role.slice(1)}</p>
-                    <p className='text-gray-900 bg-green-300 px-2 text-sm rounded-lg py-0.5'>{user.status}</p>
-                  </div>
-                </div>
-              </div>
-              <div className='mt-7 space-y-2'>
-                <p className='text-gray-600 text-sm'><span className='text-sm'>Email:</span> {user.email}</p>
-                <p className='text-gray-600 text-sm'><span className='text-sm'>Phone:</span> {user.phone}</p>
-                <p className='text-gray-600 text-sm'><span className='text-sm'>Hire Date:</span> {user.hireDate.toLocaleDateString()}</p>
-              </div>
-              <div className='mt-4 flex'>
-                <button className='w-70 border border-blue-200 rounded-md px-4 py-1 mr-2 hover:bg-blue-300'>Deactivate</button>
-                <button className='border border-blue-200 rounded-md px-4 py-1 mr-2 hover:bg-blue-300'><MdOutlineModeEdit /></button>
-                <button className='border border-red-300 rounded-md px-4 py-1 text-red-500 hover:bg-red-300'><FaRegTrashCan /></button>
-              </div>
-            </div>
+            <StaffCard key={user.id} user={user} />
           )) : <p>No staff members found.</p>
-        }
+          }
         </div>
     </div>
   )
